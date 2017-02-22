33318b6b4cca0be3d580d5bd282360e139a52f5d
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-02-22 16:04:23 +0000

    React-router

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import App from './App.jsx'
+import Application from './Application.jsx'
 
 render(
-  <App />,
+  <Application />,
   document.getElementById('app')
 )
