65592239008ddd34786bc371a95ee1678648d46d
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-02-23 14:03:18 +0000

    AVA Constantify Home
    Though couldn’t seem to test this properly, but not going to waste time
    on it

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import HomeConstants from '../constants/home-constants'
+
 export default React.createClass({
   render: function () {
     return (
       <div>
-        <h2>Home Page</h2>
+        <h2><span id={HomeConstants.homeTitleId}>{HomeConstants.homeTitleText}</span></h2>
       </div>
     )
   }
