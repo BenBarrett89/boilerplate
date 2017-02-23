f9185b6f56b90e9b4f805caf7e5c3d19d631d32f
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-02-23 11:55:22 +0000

    AVA Domain tests
    Added JSDOM, Enzyme (+requires react-addons-test-utils) and Sinon too

---
+++ client/constants/domain-constants.js
@@ -0,0 +1,19 @@
+const backToHomeLinkText = 'Back to Home'
+const counterText = 'Counter: '
+const decrementButtonId = 'decrement'
+const decrementButtonText = 'Decrement'
+const incrementButtonId = 'increment'
+const incrementButtonText = 'Increment'
+const titleTest = 'Domain Page'
+const valueSpanId = 'value'
+
+module.exports = {
+  backToHomeLinkText,
+  counterText,
+  decrementButtonId,
+  decrementButtonText,
+  incrementButtonId,
+  incrementButtonText,
+  titleTest,
+  valueSpanId
+}
