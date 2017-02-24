f96dfcca2fa321f3eadbb20ebd8829fd7895d898
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-02-24 16:06:37 +0000

    Mongoose repository tests
    Still no pattern for this :(

--- server/domains/domain/test/domain-repository-spec.js
+++ server/domains/domain/test/domain-repository-spec.js
@@ -1,3 +1,56 @@
 import test from 'ava'
+const Mongoose = require('mongoose').Mongoose
+const mongoose = new Mongoose()
+mongoose.Promise = require('bluebird')
+const mockgoose = require('mockgoose')
+const sinon = require('sinon')
 
-test.todo(`Write tests`)
+const DomainSchema = require('../../../schemas/DomainSchema')
+let DomainMock
+let domainRepository
+
+test.before(async assert => {
+  await mockgoose(mongoose)
+  await mongoose.connect('mongodb://localhost:27017/boilerplate-test')
+  const Domain = mongoose.model('Domain', DomainSchema)
+  DomainMock = sinon.mock(Domain)
+  domainRepository = require('../domain-repository').init(Domain)
+})
+
+test(`Can create a domain object with a valid domain object`, async assert => {
+  const testDomainObject = {
+    count: 5,
+    time: new Date(2017, 2, 23, 9)
+  }
+  const result = await domainRepository.createDomain(testDomainObject)
+  assert.is(result, 'Domain created', `Creates a domain object successfully`)
+})
+
+test(`Will not create a domain object with an invalid domain object (missing time)`, async assert => {
+  const testDomainObject = {
+    count: 5
+  }
+  assert.throws(domainRepository.createDomain(testDomainObject), 'Domain validation failed', `Throws the validation error up from the model`)
+})
+
+test(`Will not create a domain object with an invalid domain object (missing count)`, async assert => {
+  const testDomainObject = {
+    time: new Date(2017, 2, 23, 9)
+  }
+  assert.throws(domainRepository.createDomain(testDomainObject), 'Domain validation failed', `Throws the validation error up from the model`)
+})
+
+test.serial(`Can get domain objects`, async assert => {
+  const stubbedDomains = [
+    {count: 10, date: new Date(2017, 2, 23, 9)}
+  ]
+  DomainMock.expects('find').yields(null, stubbedDomains)
+  const result = await domainRepository.getDomains()
+  assert.is(result, stubbedDomains, `Creates a domain object successfully`)
+})
+
+test.serial.failing(`Returns the error if there is an error getting domain objects`, async assert => {
+  const expectedError = new Error()
+  DomainMock.expects('find').yields(expectedError)
+  assert.throws(domainRepository.getDomains(), expectedError, `Throws the error up from the model`)
+})
--- server/schemas/test/DomainSchema-spec.js
+++ server/schemas/test/DomainSchema-spec.js
@@ -5,21 +5,15 @@ mongoose.Promise = require('bluebird')
 const mockgoose = require('mockgoose')
 
 const DomainSchema = require('../DomainSchema')
+let Domain
 
-test.before(assert => {
-  mockgoose(mongoose).then(() => {
-    mongoose.connect('mongodb://localhost:27017/boilerplate-test', (error) => {
-      if (error) assert.fail(`Mongoose connection failed with error: ${error}`)
-    })
-  })
-})
-
-test.afterEach(assert => {
-  mongoose.connection.models = {}
+test.before(async assert => {
+  await mockgoose(mongoose)
+  await mongoose.connect('mongodb://localhost:27017/boilerplate-test')
+  Domain = mongoose.model('Domain', DomainSchema)
 })
 
 test(`Can create valid Domain objects`, async assert => {
-  const Domain = mongoose.model('Domain', DomainSchema)
   const testDomainObject = {
     count: 5,
     time: new Date(2017, 2, 23, 9)
@@ -30,7 +24,6 @@ test(`Can create valid Domain objects`, async assert => {
 })
 
 test(`Will not create invalid Domain objects (missing time)`, async assert => {
-  const Domain = mongoose.model('Domain', DomainSchema)
   const testDomainObject = {
     count: 5
   }
@@ -39,7 +32,6 @@ test(`Will not create invalid Domain objects (missing time)`, async assert => {
 })
 
 test(`Will not create invalid Domain objects (missing count)`, async assert => {
-  const Domain = mongoose.model('Domain', DomainSchema)
   const testDomainObject = {
     time: new Date(2017, 2, 23, 9)
   }
