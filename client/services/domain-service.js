43a98b3760cb7e245bbfa6e77c88c32682a2f9bf
Ben Barrett <benjamin.barrett@dwp.gsi.gov.uk>
2017-03-07 15:41:20 +0000

    Refactored sagas
    Trying to aim for dependency injection model here too. The sagas are
    still difficult to test when it comes to exceptions :(

--- client/services/domain-service.js
+++ client/services/domain-service.js
@@ -1,47 +1,52 @@
-import axiosInstance from './axios-instance'
 import { DOMAIN_PATH } from '../config/api-config'
 
-const deleteDomainService = id => {
-  return new Promise((resolve, reject) => {
-    axiosInstance.delete(`${DOMAIN_PATH}/${id}`)
-      .then(response => {
-        resolve(response.data)
-      })
-      .catch(error => {
-        console.error(error)
-        reject(error)
-      })
-  })
-}
+const domainService = axiosInstance => {
+  const deleteDomainService = id => {
+    return new Promise((resolve, reject) => {
+      axiosInstance.delete(`${DOMAIN_PATH}/${id}`)
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(error => {
+          console.error(error)
+          reject(error)
+        })
+    })
+  }
 
-const getDomainsService = () => {
-  return new Promise((resolve, reject) => {
-    axiosInstance.get(DOMAIN_PATH)
-      .then(response => {
-        resolve(response.data)
-      })
-      .catch(error => {
-        console.error(error)
-        reject(error)
-      })
-  })
-}
+  const getDomainsService = () => {
+    return new Promise((resolve, reject) => {
+      axiosInstance.get(DOMAIN_PATH)
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(error => {
+          console.error(error)
+          reject(error)
+        })
+    })
+  }
 
-const postDomainService = domain => {
-  return new Promise((resolve, reject) => {
-    axiosInstance.post(DOMAIN_PATH, domain)
-      .then(response => {
-        resolve(response.data)
-      })
-      .catch(error => {
-        console.error(error)
-        reject(error)
-      })
-  })
+  const postDomainService = domain => {
+    return new Promise((resolve, reject) => {
+      axiosInstance.post(DOMAIN_PATH, domain)
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(error => {
+          console.error(error)
+          reject(error)
+        })
+    })
+  }
+
+  return {
+    deleteDomainService,
+    getDomainsService,
+    postDomainService
+  }
 }
 
 module.exports = {
-  deleteDomainService,
-  getDomainsService,
-  postDomainService
+  init: domainService
 }
